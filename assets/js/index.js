8c85cef0c0f591c0457cf73a33cb8a16c3dc1cb3
BestMatEver <mattkun@gmail.com>
2016-02-11 18:42:23 -0500

    got media queries working, time for fleshing out page

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,32 +2,6 @@ var scriptOut1 =document.querySelector("#scriptOut1");
 var scriptOut2 =document.querySelector("#scriptOut2");
 //scriptOut1.textContent =" it is Garbage sized.";
 
-//I found this window width discovery solution on http://ryanve.com/lab/dimensions/
-//what it seems to be doing is asking the document(ie: your code)'s documentElement(its output on the screen) its width. this is what i think. I could use some clarification here.
-var width = document.documentElement.clientWidth;//this DEF gets the screen width...
-
-//console.log(width);
-width.addListener(sizeChange);//makes a listener that constantly calls sizeChange to do something. this is good for up to the milisecond page changes, like re-sizing.
-
-function sizeChange()
-{alert("inside sizechange");
-  if(width<= 320)
-  {
-    scriptOut2.textContent ="HOLY COW! We're in moble-land!"
-  }
-  else if(width <= 1024)
-  {
-    scriptOut1.textContent ="We're in Tablet Territory now!";
-  }
-  else {
-    {
-      //do nothing. we good.
-    }
-  }
-}
-
-
-
 
 //----------------BELOW SEEMS TO BE ABLE TO PRINT TO THE SCREEN BASED ON WINDOW SIZE-----------------------
 //the problem is you seem to need different listeners for going up to a certain size and down from a certain size. so in total, it would be 4 listeners, 4 media querys, and 4 functions. I think I can do it more elegantly.
@@ -44,5 +18,11 @@ function downToTab()
   {scriptOut2.textContent="WE ARE NOW TABLET SIZED!";} //if it does, write something to the screen.
   else {}  //if not do nothing.
 }//end goTab
+
+
+
+//I found this window width discovery solution on http://ryanve.com/lab/dimensions/
+//what it seems to be doing is asking the document(ie: your code)'s documentElement(its output on the screen) its width. this is what i think. I could use some clarification here.
+var width = document.documentElement.clientWidth;//this DEF gets the screen width...
 //----------------------------------------------------------------------------------------------------------
 */
